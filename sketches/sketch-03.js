4381a18ab153a48deccad23d25a22434be3b10ef
Christelle <christelle.bettoni@gmail.com>
2022-12-07 18:58:10 +0100

    Draw lines with different lineWidht between 2 animate agents (with more or less distance)

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -1,16 +1,17 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
   dimensions: [ 1080, 1080 ],
   animate: true
 };
 
-const animate = () => {
+/* const animate = () => {
   console.log("animation test");
-  requestAnimationFrame(animate); //se declenche a chaque nouvelle frame
+  requestAnimationFrame(animate); //go off on each new frame (~60/sec)
 };
-//animate(); =>cette fct fait la mm chose que la fct return plus bas
+//animate(); =>Do the same thing than return on the bottom */
 
 const sketch = ({ context, width, height }) => {
   const agents = [];
@@ -26,6 +27,27 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
+    for (let i = 0; i < agents.length; i++) { //nested for loop (draw lines between all agents)
+      const agent = agents[i];
+
+      for (let j = i + 1/* =>less iterations between i and j */; j < agents.length; j++) {
+        const other = agents[j]; //for each agent we go over other agents
+
+        const dist = agent.pos.getDistance(other.pos); //get distance between agents
+
+        if (dist > 200) continue; //draw a line only if dist > 200 or whatever
+
+        //lineWidth based on the distance between 2 agents (Between 0 = 12 & between 200 = 1)
+        context.lineWidth = math.mapRange(dist, 0, 200, 12, 1);
+
+        //draw line = moveTo & lineTo
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke();
+      }
+    }
+
     agents.forEach(agent => {
       agent.update();
       agent.draw(context);
@@ -40,6 +62,12 @@ class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+  }
+
+  getDistance(v) { //pythagore to find closest agents do draw line
+    const dx = this.x - v.x;
+    const dy = this.y - v.y;
+    return Math.sqrt(dx * dx + dy * dy);
   }
 }
 
@@ -66,7 +94,7 @@ draw(context) { //draw the dots
 
   context.lineWidth = 4; //stronger outlines
 
-  context.beginPath();
+  context.beginPath(); //new drawing
   context.arc(0, 0, this.radius, 0, Math.PI * 2);
   context.fill();
   context.stroke(); //basic black outline, no need to precise this
