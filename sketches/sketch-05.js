f9e4cca92834f4dd623ed770dba489ef9391f963
Christelle <christelle.bettoni@gmail.com>
2022-12-14 22:54:54 +0100

    center the glyph with metrics

--- sketches/sketch-05.js
+++ sketches/sketch-05.js
@@ -4,21 +4,41 @@ const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
+let text = 'B';
+let fontSize = 1200;
+let fontFamily = 'serif';
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
     context.fillStyle = 'black';
-    context.font = '400px serif';
-    context.textBaseline = 'middle'; //baseline on the middle of the txt
-    context.textAlign = 'center'; //center vertically the txt on the middle of the sketch
+    /* context.font = fontSize + 'px ' + fontFamily; */
+    context.font = `${fontSize}px ${fontFamily}`; //same than the line above
+    context.textBaseline = 'top'; //baseline on the middle of the txt
+/*     context.textAlign = 'center'; //center horizontally the txt on the middle of the sketch
+ */
+    const metrics = context.measureText(text); //const who measure the text
+/*     console.log(metrics); = console log metrics*/
+    const mx = metrics.actualBoundingBoxLeft * -1;
+    const my = metrics.actualBoundingBoxAscent * -1;
+    const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+    const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+    const x = (width - mw) * 0.5 - mx; //center the glyph
+    const y = (height - mh) * 0.5 - my;
 
     context.save(); //saves the current drawing state of the canvas by pushing it onto a stack
-    context.translate(width * 0.5, height * 0.5); //center horizontally the txt on the middle of the sketch
+    context.translate(x, y); //center vertically the txt on the middle of the sketch
 
-    context.fillText('A', 0, 0);
-    context.restore() //restores the most recently saved drawing state by popping it from the stack
+    //DRAW BOUNDING BOX AROUND THE GLYPH :
+    context.beginPath();
+    context.rect(mx, my, mw, mh);
+    context.stroke();
+
+    context.fillText(text, 0, 0);
+    context.restore(); //restores the most recently saved drawing state by popping it from the stack
   };
 };
 
