0e7d79d6db4b4f052becd93708fe77b7e9962dd6
Christelle <christelle.bettoni@gmail.com>
2022-12-16 17:35:02 +0100

    try to retrieve bitmap values 1

--- sketches/sketch-05.js
+++ sketches/sketch-05.js
@@ -10,37 +10,58 @@ let text = 'B';
 let fontSize = 1200;
 let fontFamily = 'serif';
 
-const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+const typeCanvas = document.createElement('canvas'); //create a 2nd smaller canvas
+const typeContext = typeCanvas.getContext('2D');
 
-    context.fillStyle = 'black';
-    /* context.font = fontSize + 'px ' + fontFamily; */
-    context.font = `${fontSize}px ${fontFamily}`; //same than the line above
-    context.textBaseline = 'top'; //baseline on the middle of the txt
-/*     context.textAlign = 'center'; //center horizontally the txt on the middle of the sketch
- */
-    const metrics = context.measureText(text); //const who measure the text
-/*     console.log(metrics); = console log metrics*/
+const sketch = ({ context, width, height }) => {
+  const cell = 20;
+  const cols = Math.floor(width / cell); //find the nbr of cols(in every 20px in our main canvas, we gonna have 1px in the smaller one)
+  const rows = Math.floor(height / cell);
+  const numCells = cols * rows;
+
+  typeCanvas.width  = cols;
+  typeCanvas.height = rows;
+
+  return ({ context, width, height }) => {
+    typeContext.fillStyle = 'white';
+    typeContext.fillRect(0, 0, cols, rows);
+
+    typeContext.fillStyle = 'black';
+    typeContext.font = `${fontSize}px ${fontFamily}`;
+    typeContext.textBaseline = 'top'; //baseline on the top of the txt
+
+    const metrics = typeContext.measureText(text); //const who measure the text
+    //console.log(metrics); = console log metrics
     const mx = metrics.actualBoundingBoxLeft * -1;
     const my = metrics.actualBoundingBoxAscent * -1;
     const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
     const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
 
-    const x = (width - mw) * 0.5 - mx; //center the glyph
-    const y = (height - mh) * 0.5 - my;
+    const tx = (cols - mw) * 0.5 - mx; //center the glyph
+    const ty = (rows - mh) * 0.5 - my;
 
-    context.save(); //saves the current drawing state of the canvas by pushing it onto a stack
-    context.translate(x, y); //center vertically the txt on the middle of the sketch
+    typeContext.save(); //saves the current drawing state of the canvas by pushing it onto a stack
+    typeContext.translate(tx, ty); //center vertically the txt on the middle of the sketch
 
     //DRAW BOUNDING BOX AROUND THE GLYPH :
-    context.beginPath();
-    context.rect(mx, my, mw, mh);
-    context.stroke();
+    typeContext.beginPath();
+    typeContext.rect(mx, my, mw, mh);
+    typeContext.stroke();
 
-    context.fillText(text, 0, 0);
-    context.restore(); //restores the most recently saved drawing state by popping it from the stack
+    typeContext.fillText(text, 0, 0);
+    typeContext.restore(); //restores the most recently saved drawing state by popping it from the stack
+
+    context.typeData = typeContext.getImageData(0, 0, cols, rows).data; //data property of the object
+    /* console.log(typeData); */
+
+    context.drawImage(typeCanvas, 0, 0);
+    for (let i = 0; i < numCells; i++) { //find cols & rows on a grid
+      const col = i % cols;
+      const row = Math.floor(i / cols);
+
+      const x = col * cell;
+
+    }
   };
 };
 
